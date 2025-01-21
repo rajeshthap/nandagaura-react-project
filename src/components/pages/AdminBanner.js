3eb5097e17633ef8476d35be611201d309c60e9d
rajeshthap <jdmjvm@gmail.com>
2025-01-21 17:10:14 +0530

    AWC Login page UI design

--- src/components/pages/AdminBanner.js
+++ src/components/pages/AdminBanner.js
@@ -198,17 +198,128 @@ function AdminBanner() {
                 className="nd-form-control" 
               />
             </Form.Group>
-            {/* <Form.Group className="mb-3 mt-3" controlId="block">
-              <Form.Label className="nd-form-label">Choose Block Login:</Form.Label>
+            <Form.Group className="mb-3 mt-3" controlId="block">
+              <Form.Label className="nd-form-label">District Name:</Form.Label>
               <Form.Select
                 aria-label="Choose block"
                 className="nd-steps-drop"
               >
-                <option value="">Choose Block</option>
-                <option value="Block A">Chakrata</option>
+                
+                <option selected="" disabled="">Choose District Name</option>
+                                  <option value="almora">Almora</option>
+                                  <option value="bageshwar">Bageshwar</option>
+                                  <option value="chamoli">Chamoli</option>
+                                  <option value="champawat">Champawat</option>
+                                  <option value="dehradun">Dehradun</option>
+                                  <option value="haridwar">Haridwar</option>
+                                  <option value="nanital">Nanital</option>
+                                  <option value="pauri">Pauri Garhwal</option>
+                                  <option value="pithoragarh">Pithoragarh</option>
+                                  <option value="rudraprayag">Rudraprayag</option>
+                                  <option value="tehri">Tehri Garhwal</option>
+                                  <option value="usnagar">Usnagar</option>
+                                  <option value="uttarkashi">Uttarkashi</option>
+                              
                 
               </Form.Select>
-            </Form.Group> */}
+            </Form.Group>
+
+            <Form.Group className="mb-3 mt-3" controlId="block">
+              <Form.Label className="nd-form-label">Choose Sector:</Form.Label>
+              <Form.Select
+                aria-label="Choose block"
+                className="nd-steps-drop"
+              >
+                
+                almora	<option selected="" disabled="" value="">Choose Sector</option>
+			<optgroup label="Bhaisiyachana [0506401]">
+							<option value="Badechina [01]">Badechina [01]</option>
+					<option value="SERAGHAT [02]">SERAGHAT [02]</option>
+			</optgroup><optgroup label="Bhikiyasain [0506402]">
+							<option value="BASOT[ 01] [01]">BASOT[ 01] [01]</option>
+					<option value="BHIKIYASEN [02]">BHIKIYASEN [02]</option>
+					<option value="DAULA [03]">DAULA [03]</option>
+					<option value="VINAYAK [04]">VINAYAK [04]</option>
+			</optgroup><optgroup label="Chaukhutiya [0506403]">
+							<option value="CHAUKHUTIYA[ 01] [01]">CHAUKHUTIYA[ 01] [01]</option>
+					<option value="JAURASI [02]">JAURASI [02]</option>
+					<option value="KHIDA [03]">KHIDA [03]</option>
+					<option value="MAASI [04]">MAASI [04]</option>
+			</optgroup><optgroup label="Dhauladevi [0506404]">
+							<option value="ANDOLI [01]">ANDOLI [01]</option>
+					<option value="BAMANSWAL [02]">BAMANSWAL [02]</option>
+					<option value="BHANOLI [03]">BHANOLI [03]</option>
+					<option value="DANIYADHYADI [04]">DANIYADHYADI [04]</option>
+					<option value="KALOTA [05]">KALOTA [05]</option>
+					<option value="PANUWANAULA [06]">PANUWANAULA [06]</option>
+					<option value="POKHARI [07]">POKHARI [07]</option>
+			</optgroup><optgroup label="Dwarahat [0506405]">
+							<option value="BINTA [01]">BINTA [01]</option>
+					<option value="DWARAHAT [02]">DWARAHAT [02]</option>
+					<option value="IRA BARAKHAM [03]">IRA BARAKHAM [03]</option>
+					<option value="KAFRA [04]">KAFRA [04]</option>
+					<option value="KUWALI[ 05] [05]">KUWALI[ 05] [05]</option>
+					<option value="MAJKHALI [06]">MAJKHALI [06]</option>
+					<option value="rawari mini [15]">rawari mini [15]</option>
+					<option value="aina var [23]">aina var [23]</option>
+					<option value="bhet [35]">bhet [35]</option>
+					<option value="melta [43]">melta [43]</option>
+			</optgroup><optgroup label="Havalbag [0506406]">
+							<option value="ALMORA 1 [01]">ALMORA 1 [01]</option>
+					<option value="ALMORA 2 [02]">ALMORA 2 [02]</option>
+					<option value="DOLAGHAT [03]">DOLAGHAT [03]</option>
+					<option value="HAWALBAGH [04]">HAWALBAGH [04]</option>
+					<option value="SITLAKHET [05]">SITLAKHET [05]</option>
+			</optgroup><optgroup label="Lamgarha [0506407]">
+							<option value="DHORA [01]">DHORA [01]</option>
+					<option value="JAINTI [02]">JAINTI [02]</option>
+					<option value="LAMGARHA [03]">LAMGARHA [03]</option>
+					<option value="SAHARFATAK [04]">SAHARFATAK [04]</option>
+			</optgroup><optgroup label="Salt [0506408]">
+							<option value="DEVAYAL [01]">DEVAYAL [01]</option>
+					<option value="HARARA [02]">HARARA [02]</option>
+					<option value="JALIKHAN [03]">JALIKHAN [03]</option>
+					<option value="MANILA [04]">MANILA [04]</option>
+			</optgroup><optgroup label="Syalde [0506409]">
+							<option value="DEGHAT [01]">DEGHAT [01]</option>
+					<option value="SARAIKHET [02]">SARAIKHET [02]</option>
+					<option value="SYALDE [03]">SYALDE [03]</option>
+			</optgroup><optgroup label="Tadikhet [0506410]">
+							<option value="BHUJAN [01]">BHUJAN [01]</option>
+					<option value="BILLEKH [02]">BILLEKH [02]</option>
+					<option value="CHILIYANOLA[ 03] [03]">CHILIYANOLA[ 03] [03]</option>
+					<option value="DEVLIKHET [04]">DEVLIKHET [04]</option>
+					<option value="KUNLAKHET [05]">KUNLAKHET [05]</option>
+					<option value="RANIKHET [06]">RANIKHET [06]</option>
+					<option value="TARIKHET [07]">TARIKHET [07]</option>
+			</optgroup><optgroup label="Takula [0506411]">
+							<option value="SOMESWAR1[ 01] [01]">SOMESWAR1[ 01] [01]</option>
+					<option value="SARAIKHET [02]">SARAIKHET [02]</option>
+					<option value="SOMESWER 2[ 02] [02]">SOMESWER 2[ 02] [02]</option>
+					<option value="TAKULA [03]">TAKULA [03]</option>
+	</optgroup>
+                              
+                
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3 mt-3" controlId="block">
+              <Form.Label className="nd-form-label">Anganwadi Name:</Form.Label>
+              <Form.Select
+                aria-label="Choose block"
+                className="nd-steps-drop"
+              >
+                
+                <option selected="" disabled="">Choose Anganwadi Name</option>
+                                  <option value="Anganwadi-01">Anganwadi-01</option>
+                                  <option value="Anganwadi-02">Anganwadi-02</option>
+                                  <option value="Anganwadi-03">Anganwadi-03</option>
+                                  <option value="Anganwadi-04">Anganwadi-04</option>
+                                  
+                              
+                
+              </Form.Select>
+            </Form.Group>
             <Form.Group className="mb-3" controlId="password">
               <Form.Label className="nd-form-label">Password</Form.Label>
               <Form.Control
